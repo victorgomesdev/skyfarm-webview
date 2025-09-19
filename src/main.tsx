29fcf3e06cd2827057ae472187196398e56e3877
Victor Gomes Nogueira <victorgomesnog123@gmail.com>
2025-09-18 22:35:46 -0300

    chore: Remove o código template e adiciona as libs necessárias.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import SkyFarmWebView from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <SkyFarmWebView />
   </StrictMode>,
 )
